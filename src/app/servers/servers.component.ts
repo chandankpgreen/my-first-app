84dab3525fa4013808f51fa025d69864b6ed27d7
chandank <chandank@meditab.com>
2018-03-16 18:30:55 +0530

    two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  
+
   allowNewServer: boolean = false;
   serverCreationStatus : string = 'No Server was created';
+  serverName: string = '';
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +22,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus =  'A server was created';
+  }
+
+  updateServer( event: any){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
